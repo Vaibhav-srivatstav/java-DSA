7ba41fd137c815cbee81d78744ca7f3865fc74b5
VAIBHAV SRIVASTAVA <srivaibhav630@gmail.com>
2026-07-28 09:12:00 +0530

    Time: 5 ms (80.78%), Space: 46.2 MB (16.74%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,18 +1,22 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if (x < 0)
-            return false;
-        int reverse = 0;
-        int original = x;
-        while (x != 0) {
-            int lastdigit = x % 10;
-            reverse = reverse * 10 + lastdigit;
-            x = x / 10;
-        }
-        if (original == reverse) {
-            return true;
-        } else {
-            return false;
-        }
+        if(x<0)return false;
+       int rev = 0;
+       int temp = x;
+       while(x!=0) {
+        int digit = x%10;
+        if (rev > Integer.MAX_VALUE / 10 ||
+                (rev == Integer.MAX_VALUE / 10 && digit > 7)) {
+                return false;
+            }
+
+            // Check negative overflow
+            if (rev < Integer.MIN_VALUE / 10 ||
+                (rev == Integer.MIN_VALUE / 10 && digit < -8)) {
+                return false;
+            }
+        rev = rev*10+digit;
+        x=x/10;
+       }return temp == rev;
     }
 }
