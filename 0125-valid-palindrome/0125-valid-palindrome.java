8b61fe1d44dc5336007d7f8d78a7d299eedffc77
VAIBHAV SRIVASTAVA <srivaibhav630@gmail.com>
2026-08-11 10:15:28 +0530

    Time: 1 ms (100%), Space: 44 MB (91.86%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,14 +1,22 @@
 class Solution {
-    public boolean revpalindrome(int i , String s){
-        if(i>=s.length()/2){
-            return true;
-        }
-
-        if(s.charAt(i)!= s.charAt(s.length()-i-1))return false;
-        return revpalindrome(i+1,s.toLowerCase());
-    }
     public boolean isPalindrome(String s) {
-        s= s.replaceAll("[^a-zA-Z0-9]", "");
-        return revpalindrome(0,s.toLowerCase());
+        int left = 0 , right = s.length()-1;
+        while(left<=right){
+            char a= s.charAt(left);
+            char b = s.charAt(right);
+            if(a>='A'&& a<='Z')a = (char)(a+32);
+            if(b>='A'  && b<='Z')b= (char)(b+32);
+            if(!((a>='a'&& a<='z') ||(a>='0' && a<='9'))){
+                left++;
+                continue;
+            }
+            if(!((b>='a' && b<='z')||(b>='0' && b<='9'))){
+                right--;
+                continue;
+            }
+            if(a!=b)return false;
+            left++;
+            right--;
+        }return true;
     }
 }
