3cab15c784fbba0a21bbc5c29ff10edbce90a2ec
VAIBHAV SRIVASTAVA <srivaibhav630@gmail.com>
2026-08-20 19:43:08 +0530

    Time: 4 ms (5.03%), Space: 268.9 MB (5.04%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,13 +1,5 @@
 class Solution {
-    void reveseArray(int[] nums, int start, int end){
-        while(start<end){
-            int temp = nums[start];
-            nums[start]= nums[end];
-            nums[end]= temp;
-            start++;
-            end--;
-        }
-    }
+
     public void rotate(int[] nums, int k) {
         int n = nums.length;
         k = k%n;
@@ -15,5 +7,14 @@ class Solution {
         reveseArray(nums, 0 , n-1);
         reveseArray(nums, 0 , k-1);
         reveseArray(nums, k , n-1);
+    }
+       private void reveseArray(int[] nums, int start, int end){
+        while(start<end){
+            int temp = nums[start];
+            nums[start]= nums[end];
+            nums[end]= temp;
+            start++;
+            end--;
+        }
     }
 }
